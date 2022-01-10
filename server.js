5d4e40b06f22d973ff9a8ea02cabb192d2f4c6a8
Mohamed Ali <12maali27@gmail.com>
2022-01-10 20:10:14 +0000

    add require statements for cookie-parser and cookie-session but commented out that along with the login route

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+// const cookieSession = require("cookie-session");
+// const cookieParser = require("cookie-parser");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -37,11 +39,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const propertiesRoutes = require("./routes/properties");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/properties", propertiesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -51,6 +55,19 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// Login
+// app.get("/login/:id", (req, res) => {
+//   // cookie-session middleware
+//   console.log(res);
+//   req.session.user_id = req.params.id;
+
+//   // cookie-parser middleware
+//   res.cookie("user_id", req.params.id);
+
+//   // send the user somewhere
+//   res.redirect("/properties");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);

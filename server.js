81d6149749637df3087a50c84b21bc4d51f73642
Trigun Joshi <trigunjoshi@gmail.com>
2022-01-10 13:08:58 -0800

    added addProperties function

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const morgan = require("morgan");
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
-db.connect();
+db.connect()
+  .then(() => {
+    console.log("Connection passed");
+  })
+  .catch((err) => {
+    console.log("I am error", err);
+  });
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.

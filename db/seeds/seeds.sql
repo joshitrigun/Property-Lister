eb44cb3df36cc8814df86d223a0627178d1c7ced
Trigun Joshi <trigunjoshi@gmail.com>
2022-01-10 10:10:28 -0800

    changes in schema and addition of seeds

---
+++ db/seeds/seeds.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO users (id, name, email, password, phone, is_admin) VALUES
+(1, 'Trigun', 'trigun@gmail.com', 'abc', '2347891234', true),
+(2, 'Mohammed', 'mohammed@gmail.com', 'abc', '33347891234', true),
+(3, 'Michael', 'mike@gmail.com', 'abc', '4047891234', true),
+(4, 'Naruto', 'trigun@gmail.com', 'abc', '5127891234', false),
+(5, 'harry', 'harry@gmail.com', 'abc', '6047891234', false),
+(6, 'carey', 'carey@gmail.com', 'abc', '3237891234', false),
+(7, 'Apple', 'apple@gmail.com', 'abc', '6042891234', false),
+(8, 'Jack', 'jack@gmail.com', 'abc', '7122891234', false),
+(9, 'Kim', 'kim@gmail.com', 'abc', '6044894534', false);
+
+INSERT INTO properties
+(owner_id, name, title, description, cost, image_url, number_of_bedrooms, number_of_bathrooms, country, province, city, postal_code, street, isActive) VALUES
+(1, 'Richmond Mansion', 'BC Mansion', 'Lorem ipsum dolor sit amet', 2000000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg', 10, 16, 'Canada', 'BC', 'Richmond', 'Y6Y5C6', 'Granville', 'true'),
+(1, 'Vancouver Mansion', 'Van Mansion', 'Lorem ipsum dolor sit amet', 4000000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg', 11, 14, 'Canada', 'BC', 'Vancouver', 'Y6Y5C6', 'A', 'true'),
+(2, 'Calgary Mansion', 'Cal Mansion', 'Lorem ipsum dolor sit amet', 100000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg', 8, 5, 'Canada', 'BC', 'Cal', 'Y6Y5C6', 'B', 'true'),
+(3, 'Toronto Mansion', 'Tor Mansion', 'Lorem ipsum dolor sit amet', 800000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg', 9, 9, 'Canada', 'BC', 'Tor', 'Y6Y5C6', 'C', 'true'),
+(4, 'Victoria Mansion', 'Vic Mansion', 'Lorem ipsum dolor sit amet', 1500000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg', 10, 11, 'Canada', 'BC', 'Victoria', 'Y6Y5C6', 'D', 'true'),
+(5, 'Alberta Mansion', 'Alberta Rentals', 'Lorem ipsum dolor sit amet', 1000000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg', 10, 12, 'Canada', 'BC', 'Alberta', 'Y6Y5C6', 'E', 'true'),
+(5, 'Montreal Mansion', 'MnM Mansion', 'Lorem ipsum dolor sit amet', 900000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg',8, 6, 'Canada', 'BC', 'Montreal', 'Y6Y5C6', 'F', 'true'),
+(8, 'Surrey Mansion', 'Surrey Mansion', 'Lorem ipsum dolor sit amet', 600000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg',5, 8, 'Canada', 'BC', 'Surrey', 'Y6Y5C6', 'G', 'true'),
+(9, 'Burnaby Mansion', 'Burnaby Housing', 'Lorem ipsum dolor sit amet', 900000, 'https://mediavault.point2.com/p2h/listing/873f/6e2b/0cab/5af3411a8de4a7fa0426/nwm_full.jpg',8, 6, 'Canada', 'BC', 'Burnaby', 'Y6Y5C6', 'H', 'true');
+
+

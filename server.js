e70e39c565d5cd857c16098f3ba8cb139489ba07
Trigun Joshi <trigunjoshi@gmail.com>
2022-01-10 14:46:10 -0800

    getProperties destructured

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const { getProperties, addProperties } = require("./routes/properties");
+const loginRoutes = require("./routes/login");
+
 // const cookieSession = require("cookie-session");
 // const cookieParser = require("cookie-parser");
 
@@ -45,14 +48,12 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 // const usersRoutes = require("./routes/users");
 // const widgetsRoutes = require("./routes/widgets");
-const propertiesRoutes = require("./routes/properties");
-const loginRoutes = require("./routes/login");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // app.use("/api/users", usersRoutes(db));
 // app.use("/api/widgets", widgetsRoutes(db));
-app.use("/properties", propertiesRoutes(db));
+app.use("/properties", getProperties(db));
 app.use("/login", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
 

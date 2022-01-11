7deeaa195a0ea865f8f1cc735b388f5f322779e0
Trigun Joshi <trigunjoshi@gmail.com>
2022-01-10 20:39:54 -0800

    changes in server, properties and app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 // Client facing scripts here
 $(document).ready(function () {
-  $("div .card").on("click", function (e) {
+  $("div.card").on("click", function (e) {
     const id = $(this).attr("id");
     window.location.href = `http://localhost:8080/properties/${id}`;
   });

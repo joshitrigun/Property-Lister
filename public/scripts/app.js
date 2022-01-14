b4ca40748fd60a2de69773e50b1ea53ebbaf752f
Mohamed Ali <12maali27@gmail.com>
2022-01-14 01:00:02 +0000

    remove click event

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,12 +5,6 @@ $(document).ready(function () {
     window.location.href = `http://localhost:8080/properties/${id}`;
   });
 
-  $("#message-submit-btn").on("click", function (e) {
-    e.preventDefault();
-    $(".message-text").val("");
-    $(".success-message").text("Message Sent!");
-  });
-
   $("#main .delete").each((element) => {
     element = $("#main .delete")[element];
     console.log("element", element);
